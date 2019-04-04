2e18f10fcbab0776ba91bcf1184400e1ad8b5c20
Carl Bordum Hansen <carl@bordum.dk>
2019-04-04 14:04:21 +0200

    Add LICENSE

--- delay_agent.py
+++ delay_agent.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2019 Magenta ApS, https://magenta.dk.
+# Contact: info@magenta.dk.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import datetime
 import dateutil.parser
 from functools import partial
--- test_consumer.py
+++ test_consumer.py
@@ -5,6 +5,13 @@ It does not require anything to run. PostgreSQL and RabbitMQ are
 mocked.
 """
 
+# Copyright (C) 2019 Magenta ApS, https://magenta.dk.
+# Contact: info@magenta.dk.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import datetime
 import json
 import re
