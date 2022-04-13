57465d049d7538d6690b6b0bbfcc76b035e5737d
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2022-04-13 11:58:06 +0200

    Add license and reuse pre-commit

--- mo_delay_agent/delay_agent.py
+++ mo_delay_agent/delay_agent.py
@@ -1,9 +1,6 @@
-# Copyright (C) 2019 Magenta ApS, https://magenta.dk.
-# Contact: info@magenta.dk.
+# SPDX-FileCopyrightText: 2019-2020 Magenta ApS
 #
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
 import datetime
 import json
 import logging
