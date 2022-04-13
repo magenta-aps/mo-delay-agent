d43c1ff0cc9a5f58f8b55b3bdb2981b82b19f70d
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2022-04-13 11:44:34 +0200

    No parent test class. monkeypatch only where needed.

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,38 +7,35 @@ env_exchange = "dummy_exchange"
 env_amqp_url = "amqp://This:is@aTest:5672"
 
 
-@pytest.fixture
-def mock_env(monkeypatch):
+def test_default_settings():
+    settings = Settings()
+    assert settings.amqp_url == "amqp://guest:guest@localhost:5672"
+    assert settings.amqp_exchange == "os2mo"
+    assert settings.amqp_delayed_exchange == "os2mo_delayed"
+    assert (
+        settings.postgresurl
+        == "postgres://delay_agent:delay_agent@localhost:5432/delay_agent"
+    )
+
+
+def test_env_override(monkeypatch):
     monkeypatch.setenv("amqp_url", env_amqp_url)
     monkeypatch.setenv("amqp_exchange", env_exchange)
+    settings = Settings()
+    assert settings.amqp_exchange == env_exchange
+    assert settings.amqp_url == env_amqp_url
 
 
-class TestSettings:
-    def test_default_settings(self):
-        settings = Settings()
-        assert settings.amqp_url == "amqp://guest:guest@localhost:5672"
-        assert settings.amqp_exchange == "os2mo"
-        assert settings.amqp_delayed_exchange == "os2mo_delayed"
-        assert (
-            settings.postgresurl
-            == "postgres://delay_agent:delay_agent@localhost:5432/delay_agent"
-        )
-
-    def test_env_override(self, mock_env):
-        settings = Settings()
-        assert settings.amqp_exchange == env_exchange
-        assert settings.amqp_url == env_amqp_url
-
-    @pytest.mark.parametrize(
-        "setting,value",
-        [
-            ("amqp_url", "Not a amqp url"),
-            ("postgres_url", "Not a postgres url"),
-            ("amqp_exchange", 1),
-            ("amqp_delayed_exchange", 1),
-            ("not_a_setting", "Should also fail"),
-        ],
-    )
-    def test_wrong_settings_type(self, setting, value):
-        with pytest.raises(ValidationError):
-            Settings(setting=value)
+@pytest.mark.parametrize(
+    "setting,value",
+    [
+        ("amqp_url", "Not a amqp url"),
+        ("postgres_url", "Not a postgres url"),
+        ("amqp_exchange", 1),
+        ("amqp_delayed_exchange", 1),
+        ("not_a_setting", "Should also fail"),
+    ],
+)
+def test_wrong_settings_type(setting, value):
+    with pytest.raises(ValidationError):
+        Settings(setting=value)
